fa47f7487397110fd0ecdb5c973dff6585d337ef
Vikash Mitruka <vikash.mitruka@gmail.com>
2013-11-02 10:45:31 +0530

    Routing of Web API (As per the specs of APS)

--- Illuminate.Web.API/App_Start/WebApiConfig.cs
+++ Illuminate.Web.API/App_Start/WebApiConfig.cs
@@ -9,11 +9,23 @@ namespace Illuminate.Web.API
     {
         public static void Register(HttpConfiguration config)
         {
+            //POST contribute/content/3 -- Update the Content with Id = 3
+            //POST contribute/content -- Create a new content
             config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
+                name: "ContributeApi",
+                routeTemplate: "contribute/{controller}/{contentId}", defaults: new { contentId = RouteParameter.Optional }
             );
+
+            //consume/content -- List all content
+            //consume/content/3 -- details of content with id = 3
+            config.Routes.MapHttpRoute(
+                name: "ConsumeListContent",
+                routeTemplate: "consume/{controller}/{contentId}", defaults: new { contentId = RouteParameter.Optional }
+            );
+
+            //Ignoring the XML serialization, to allow the Chrome to use the JSON format by default
+            var appXmlType = config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml");
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType);
         }
     }
 }
