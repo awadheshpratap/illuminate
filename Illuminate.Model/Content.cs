8e1566b8faa75ba8581273756739e72fe322703d
Vikash Mitruka <vikash.mitruka@gmail.com>
2013-11-02 10:45:12 +0530

    Content Data Model Update

--- Illuminate.Model/Content.cs
+++ Illuminate.Model/Content.cs
@@ -8,10 +8,11 @@ namespace Illuminate.Model
     public class Content
     {
         public int Id { get; set; }
+        public string Title { get; set; }
+        public int CategoryId { get; set; }
         public string Author { get; set; }
         public string Description { get; set; }
-        public int CategoryId { get; set; }
-        public string Value { get; set; }
+        public string Data { get; set; }
         public string ContentUri { get; set; }
         public string CreatorRef { get; set; }
         public string UpdaterRef { get; set; }
