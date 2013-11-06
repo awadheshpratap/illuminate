1ed78a2997ab30ee880ef8af4b13a52935f59517
awadheshpratap <awadhesh.iiitm@gmail.com>
2013-11-07 00:55:15 +0530

    Create content page, angular js parts

---
+++ Illuminate.Web/Scripts/application/createcontent.controller.js
@@ -0,0 +1,34 @@
+﻿var myApp = angular.module('content',[]);
+ 
+myApp.controller('CreateContentCtrl', function ($scope, $http) {
+    var baseUrl = 'http://localhost:8081';
+
+    $scope.area = "";
+    $scope.topic = "";
+    $scope.author = "";
+    $scope.title = "";
+    $scope.description = "";
+    $scope.fileName = "";
+
+    $scope.publish = function () {
+
+        $scope.content = {
+            "Id":-1, 
+            "Title": $scope.title,
+            "CategoryId": 1,
+            "Description": $scope.description
+        };
+
+        $http({
+            url: baseUrl + '/contribute/content/-1',
+            method: "POST",
+            data: JSON.stringify($scope.content),
+            headers: {'Content-Type': 'application/json'}
+        }).success(function (data, status, headers, config) {
+             
+        }).error(function (data, status, headers, config) {
+            $scope.status = status + ' ' + headers;
+        });
+    }; 
+
+});
