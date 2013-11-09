0d23bea75071cb8f8ac07ed10e3c6648c20c1190
awadheshpratap <awadhesh.iiitm@gmail.com>
2013-11-10 00:10:39 +0530

    EF : Corrections in ContentLike & ContentComment entities

--- Illuminate.Model/Content.Designer.cs
+++ Illuminate.Model/Content.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Default code generation is disabled for model 'D:\Applications\illuminate\trunk\Illuminate.Model\Content.edmx'. 
+﻿// Default code generation is disabled for model 'D:\Github\illuminate\Illuminate.Model\Content.edmx'. 
 // To enable default code generation, change the value of the 'Code Generation Strategy' designer
 // property to an alternate value. This property is available in the Properties Window when the model is
 // open in the designer.
